a712eb46da5f8ec8cf51f66b8d8d3ddf877c117c
Kenny Yang <kennyyang19@gmail.com>
2019-07-16 02:12:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 
-function takeANumber(katzDeli, name) {
-  katzDeli.push(name)
-  var position = katzDeli.length
-  return 'Welcome, ' + name + '. You are number ' + position + ' in line.'
+function takeANumber(katzDeli) {
+  var i=0
+  i++
+  return 'Welcome, ' + i
 }
 
 
 
 function nowServing(deliLine) {
-  if (deliLine.length === 0) { 
-    return 'There is nobody waiting to be served!'
-  }
-
-  else { 
-  var position = deliLine[0]
-  deliLine.shift();
-  return 'Currently serving ' + position + '.'
-  }
+ if (deliLine.length === 0) {
+   return 'There is nobody waiting to be served!'
+ }
+ else { 
+   var position = deliLine[0]
+   deliLine.shift();
+   return 'Currenty serving ' + position + '.'
+   
+ }
 }
 
 
 
 function currentLine(line) {
-  if (line.length === 0) { 
+  if (line.length===0) {
     return 'The line is currently empty.'
-}
+  }
   else {
-    return 'The line is currently: ' + '1. ' + line[0] + ', 2. ' + line[1] + ', 3. ' + line[2]
+    return 'The line is currently: ' + '1. ' + line[0] + ' , 2. ' + line[1] + ' , 3. ' + line[2]
   }
 }
 
